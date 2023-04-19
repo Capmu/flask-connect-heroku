5f7fd20d52ce83c50da1790f3a2a815148d2289a
Capmu <p.kanawat.capmu@gmail.com>
2023-04-19 10:01:16 +0700

    Add basic auth for private API

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 from flask import Flask
+from flask_httpauth import HTTPBasicAuth
 
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+USER_DATA = {
+    "admin": "1234"
+}
+
+
+@auth.verify_password
+def verify(username, password):
+    if not (username and password):
+        return False
+    return USER_DATA.get(username) == password
 
 
 @app.route('/')
 def home():
     return "Flask heroku app."
+
+@app.route('/private')
+@auth.login_required
+def private():
+    return "Flask heroku app. (private)"
 
 
 if __name__ == '__main__':
